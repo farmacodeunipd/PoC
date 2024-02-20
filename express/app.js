05bb026f925c7f38056ce8a8396a3ca5e76d2c1e
Ciro9320 <ciropassata93@gmail.com>
2024-02-20 11:29:41 +0100

    express worka in locale

--- express/app.js
+++ express/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mysql = require("mysql");
+require("dotenv").config();
 
 const app = express();
-const port = 3333;
+const port = 5000;
 
 const connection = mysql.createConnection({
-    host: "db",
+    host: "localhost",
     user: "myuser",
     password: "mypassword",
     database: "mydatabase",
+    port: 3306,
 });
 
 connection.connect((err) => {
@@ -27,7 +29,7 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
     connection.query(
-        "SELECT cod_cli, rag_soc FROM anacli LIMIT 10",
+        "SELECT cod_cli, rag_soc FROM anacli LIMIT 20",
         (error, results, fields) => {
             if (error) {
                 console.error(
@@ -48,4 +50,4 @@ app.listen(port, () => {
     console.log(`Server Express in esecuzione sulla porta ${port}`);
 });
 
-connection.end();
+// connection.end();
