fd955be59e485ac8bd955f696afbf6cb97ed589b
rossonlorenzo <rossonlorenzo@gmail.com>
2024-02-28 18:22:59 +0100

    test

--- test/algo.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def test_python_api():
-    response = requests.get('http://localhost:4000')
-    assert response.status_code == 200
-    print("Python API Test Passed!")
--- test/express.js
+++
@@ -1,4 +0,0 @@
-test('Express API Test', async () => {
-    const response = await fetch('http://localhost:3080');
-    expect(response.status).toBe(200);
-  });
