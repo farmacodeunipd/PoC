825abd0c62c020a70c8614a1c564c78449b38a30
rossonlorenzo <rossonlorenzo@gmail.com>
2024-03-25 15:36:02 +0100

    linting test

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,35 +1,36 @@
 module.exports = {
     "env": {
-      "browser": true,
-      "node": true, // Include Node.js environment
-      "es2021": true,
-      "jest": true, // Include Jest environment
-      "mocha": true // Include Mocha environment
+        "browser": true,
+        "node": true,
+        "es2021": true,
+        "jest": true,
+        "mocha": true
     },
     "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended"
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
     "overrides": [
-      {
-        "files": [
-          "**/__tests__/*.{js,jsx}" // Override settings for test files
-        ],
-        "env": {
-          "jest": true,
-          "mocha": true
+        {
+            "files": [
+                "**/__tests__/*.{js,jsx}"
+            ],
+            "env": {
+                "jest": true,
+                "mocha": true
+            }
         }
-      }
     ],
     "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "plugins": [
-      "react"
+        "react"
     ],
     "rules": {
-      // Define any custom rules or override existing rules here
+        // Define any custom rules or override existing rules here
     }
-  }
+}
+
   
